2df4b69efb93dadaa9c21c170df3addd3e3df0a8
nam <namhoangpham96@gmail.com>
2022-01-19 03:56:58 -0500

    Add operation button to the app and fix typos

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import DigitButton from './DigitButton';
 import OperationButton from './OperationButton';
-import useReducer from "react"
+import {useReducer} from "react"
 
 export const ACTIONS = {
   ADD_DIGIT: 'add_digit',
   CLEAR: 'clear',
-  CHOOSE_OPEARION: 'choose_operation',
+  CHOOSE_OPERATION: 'choose_operation',
   DELETE_DIGIT: 'delete_digit',
   EVALUATE: 'evaluate'
 }
 
-function reducer(state, {type, payload}){
+function reducer( state, {type, payload} ){
   switch(type) {
     case ACTIONS.ADD_DIGIT:
       return {
@@ -37,25 +37,25 @@ function App() {
       <button className="span2">AC</button>
       <button>DEL</button>
 
-      <OperationButton digit=":" dispatch={dispatch} />
+      <OperationButton operation=":" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
 
-      <OperationButton digit="*" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
 
-      <OperationButton digit="+" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
 
-      <OperationButton digit="-" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      
+
       <button className="span2">=</button>
     </div>
   );
