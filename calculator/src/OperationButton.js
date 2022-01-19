c78ba0c3c655269ed59b4b22f6b22c6f0de022b1
nam <namhoangpham96@gmail.com>
2022-01-19 03:55:21 -0500

    fix button function

--- calculator/src/OperationButton.js
+++ calculator/src/OperationButton.js
@@ -1,7 +1,9 @@
 import {ACTIONS} from './App'
 
 export default function OperationButton({dispatch, operation}) {
-    return  <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPEARION, payload: {operation} })}>
+    return (
+        <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation} })}>
                 {operation}
-            </button>
+        </button>
+    )  
 }
