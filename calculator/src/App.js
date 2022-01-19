6eda2ef2c5b58145f44b5db24ab07456dd3db26a
nam <namhoangpham96@gmail.com>
2022-01-19 06:01:07 -0500

    Overwrite can add new number

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer( state, {type, payload} ){
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
